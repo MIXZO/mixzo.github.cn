a194da1ba81a958813e91312c1568e17abc8d90a
ZO <mixzo@vip.qq.com>
2015-03-09 18:00:09 +0800

    201503091759
    chapters-01 step-1 finish

---
+++ js/chapters-01.js
@@ -0,0 +1,182 @@
+﻿var chapters01 = (function($) {
+	var chapters01Fun = {
+			chapters_01: function() {	//chapters 01
+				common.pageLoad(function() {
+					$('.chapters-box, .step-1').css('display', 'block').transition({
+						'opacity': '1'
+					});
+				});
+
+				var canvasFun = function() {
+					var canvas = document.getElementById("cas_pb"),
+						ctx = canvas.getContext("2d");
+					var x1, y1, a = 30,
+						timeout, totimes = 100,
+						jiange = 30;
+					canvas.width = $('body').width();
+					canvas.height = $('body').height();
+					var img = new Image();
+					img.src = "../images/img/chapters_01_00.jpg";
+					img.style.backgroundSize = 'cover';
+					img.onload = function() {
+						ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+						tapClip();
+					}
+
+					//通过修改globalCompositeOperation来达到擦除的效果  
+					function tapClip() {
+						var hastouch = "ontouchstart" in window ? true : false,
+							tapstart = hastouch ? "touchstart" : "mousedown",
+							tapmove = hastouch ? "touchmove" : "mousemove",
+							tapend = hastouch ? "touchend" : "mouseup";
+
+						ctx.lineCap = "round";
+						ctx.lineJoin = "round";
+						ctx.lineWidth = a * 2;
+						ctx.globalCompositeOperation = "destination-out";
+
+						canvas.addEventListener(tapstart, function(e) {
+							clearTimeout(timeout)
+							e.preventDefault();
+
+							x1 = hastouch ? e.targetTouches[0].pageX : e.clientX - canvas.offsetLeft;
+							y1 = hastouch ? e.targetTouches[0].pageY : e.clientY - canvas.offsetTop;
+
+							ctx.save();
+							ctx.beginPath()
+							ctx.arc(x1, y1, 1, 0, 2 * Math.PI);
+							ctx.fill();
+							ctx.restore();
+
+							canvas.addEventListener(tapmove, tapmoveHandler);
+							canvas.addEventListener(tapend, function() {
+								canvas.removeEventListener(tapmove, tapmoveHandler);
+
+								timeout = setTimeout(function() {
+									var imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+									var dd = 0;
+									for (var x = 0; x < imgData.width; x += jiange) {
+										for (var y = 0; y < imgData.height; y += jiange) {
+											var i = (y * imgData.width + x) * 4;
+											if (imgData.data[i + 3] > 0) {
+												dd++
+											}
+										}
+									}
+									if (dd / (imgData.width * imgData.height / (jiange * jiange)) < 0.4) {
+										$('#cas_pb').fadeOut('slow');
+									}
+								}, totimes)
+							});
+
+							function tapmoveHandler(e) {
+								clearTimeout(timeout)
+								e.preventDefault()
+								x2 = hastouch ? e.targetTouches[0].pageX : e.clientX - canvas.offsetLeft;
+								y2 = hastouch ? e.targetTouches[0].pageY : e.clientY - canvas.offsetTop;
+
+								ctx.save();
+								ctx.moveTo(x1, y1);
+								ctx.lineTo(x2, y2);
+								ctx.stroke();
+								ctx.restore()
+
+								x1 = x2;
+								y1 = y2;
+							}
+						})
+					}
+
+					//使用clip来达到擦除效果  
+					function otherClip() {
+						var hastouch = "ontouchstart" in window ? true : false,
+							tapstart = hastouch ? "touchstart" : "mousedown",
+							tapmove = hastouch ? "touchmove" : "mousemove",
+							tapend = hastouch ? "touchend" : "mouseup";
+
+						canvas.addEventListener(tapstart, function(e) {
+							clearTimeout(timeout)
+							e.preventDefault();
+
+							x1 = hastouch ? e.targetTouches[0].pageX : e.clientX - canvas.offsetLeft;
+							y1 = hastouch ? e.targetTouches[0].pageY : e.clientY - canvas.offsetTop;
+
+							ctx.save()
+							ctx.beginPath()
+							ctx.arc(x1, y1, a, 0, 2 * Math.PI);
+							ctx.clip()
+							ctx.clearRect(0, 0, canvas.width, canvas.height);
+							ctx.restore();
+
+							canvas.addEventListener(tapmove, tapmoveHandler);
+							canvas.addEventListener(tapend, function() {
+								canvas.removeEventListener(tapmove, tapmoveHandler);
+
+								timeout = setTimeout(function() {
+									var imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+									var dd = 0;
+									for (var x = 0; x < imgData.width; x += jiange) {
+										for (var y = 0; y < imgData.height; y += jiange) {
+											var i = (y * imgData.width + x) * 4;
+											if (imgData.data[i + 3] > 0) {
+												dd++
+											}
+										}
+									}
+									if (dd / (imgData.width * imgData.height / (jiange * jiange)) < 0.4) {
+										canvas.className = "noOp";
+									}
+								}, totimes)
+
+							});
+
+							function tapmoveHandler(e) {
+								e.preventDefault()
+								x2 = hastouch ? e.targetTouches[0].pageX : e.clientX - canvas.offsetLeft;
+								y2 = hastouch ? e.targetTouches[0].pageY : e.clientY - canvas.offsetTop;
+
+								var aasin = a * Math.sin(Math.atan((y2 - y1) / (x2 - x1)));
+								var aacos = a * Math.cos(Math.atan((y2 - y1) / (x2 - x1)));
+								var x3 = x1 + asin;
+								var y3 = y1 - acos;
+								var x4 = x1 - asin;
+								var y4 = y1 + acos;
+								var x5 = x2 + asin;
+								var y5 = y2 - acos;
+								var x6 = x2 - asin;
+								var y6 = y2 + acos;
+
+								ctx.save()
+								ctx.beginPath()
+								ctx.arc(x2, y2, a, 0, 2 * Math.PI);
+								ctx.clip()
+								ctx.clearRect(0, 0, canvas.width, canvas.height);
+								ctx.restore();
+
+								ctx.save()
+								ctx.beginPath()
+								ctx.moveTo(x3, y3);
+								ctx.lineTo(x5, y5);
+								ctx.lineTo(x6, y6);
+								ctx.lineTo(x4, y4);
+								ctx.closePath();
+								ctx.clip()
+								ctx.clearRect(0, 0, canvas.width, canvas.height);
+								ctx.restore();
+
+								x1 = x2;
+								y1 = y2;
+							}
+						})
+					}
+				}
+				canvasFun();
+
+				var step2 = function() {
+				}
+			}
+		}
+		//共用调用
+	chapters01Fun.chapters_01();
+	return chapters01Fun;
+})(jQuery)
--- js/common.js
+++ js/common.js
@@ -1,154 +1,196 @@
-﻿var common = (function($) {
+﻿(function($) { //插件模块
+	$.fn.wrapAnimation = function() {
+		$(this).find('.item').each(function() {
+			var _this = $(this),
+				_act = JSON.parse("" + _this.attr('act').replace(/'/g, '"') + "");
+
+			for (var i = 0; i < _act.style.length; i++) {
+				_this.transition(_act.style[i]);
+			};
+		});
+	}
+})(jQuery);
+
+var common = (function($) {
 	var commonFun = {
-			viewportFix: function() {
-				var v_t;
-				var _width = 640 //PSD width
-
-				var heightFix = function() { //框架高度
-					$('#pageWrap').height('100%');
+		showLoad: function() { //显示load
+			$('#loading').fadeIn('slow');
+		},
+		hideLoad: function(callback) { //隐藏load
+			$('#loading').fadeOut('slow', function() {
+				if (typeof callback == 'function') {
+					callback();
+				};
+			});
+		},
+		pageLoad: function(callback) { //页面加载动作
+			$('#loading').fadeIn('slow', function() {
+				document.onreadystatechange = function() {
+					if (document.readyState == "complete") {
+						commonFun.hideLoad(callback);
+					}
 				}
+			});
+		},
+		tsIcoNext: function() { //提示上滑
+			var b_t;
+			var bottomFun = function() {
+				$('#bottom').hide();
+				clearTimeout(b_t);
+				b_t = setTimeout(function() {
+					if (!$('.step-box').last().hasClass('show')) {
+						$('#bottom').fadeIn('slow');
+					};
+				}, 3000);
+			}
+		},
+		viewportFix: function() { //屏幕适配
+			var v_t;
+			var _width = 640 //PSD width
 
-				var advice = function() { //viewport width
-					var _viewport = $('[name=viewport]');
-					var phoneWidth = parseInt(window.screen.width);
-					var phoneScale = phoneWidth / _width;
-					var ua = navigator.userAgent;
-					if (/Android (\d+\.\d+)/.test(ua)) {
-						var version = parseFloat(RegExp.$1);
-						// andriod 2.3
-						if (version > 2.3) {
-							_viewport.attr('content', 'width=' + _width + ', minimum-scale = ' + phoneScale + ', maximum-scale = ' + phoneScale + ', target-densitydpi=device-dpi');
-							// andriod 2.3以上
-						} else {
-							_viewport.attr('content', 'width=' + _width + ', target-densitydpi=device-dpi');
-						}
-						// 其他系统
+			var heightFix = function() { //框架高度
+				$('#pageWrap').height('100%');
+			}
+
+			var advice = function() { //viewport width
+				var _viewport = $('[name=viewport]');
+				var phoneWidth = parseInt(window.screen.width);
+				var phoneScale = phoneWidth / _width;
+				var ua = navigator.userAgent;
+				if (/Android (\d+\.\d+)/.test(ua)) {
+					var version = parseFloat(RegExp.$1);
+					// andriod 2.3
+					if (version > 2.3) {
+						_viewport.attr('content', 'width=' + _width + ', minimum-scale = ' + phoneScale + ', maximum-scale = ' + phoneScale + ', target-densitydpi=device-dpi');
+						// andriod 2.3以上
 					} else {
-						_viewport.attr('content', 'width=640, user-scalable=no, target-densitydpi=device-dpi');
+						_viewport.attr('content', 'width=' + _width + ', target-densitydpi=device-dpi');
 					}
+					// 其他系统
+				} else {
+					_viewport.attr('content', 'width=640, user-scalable=no, target-densitydpi=device-dpi');
+				}
 
-					heightFix();
+				heightFix();
 
-					if (window.orientation != undefined) {
-						if (window.orientation == 0) {
-							$('#advice').hide();
-						} else {
-							$('#advice').show();
-						}
+				if (window.orientation != undefined) {
+					if (window.orientation == 0) {
+						$('#advice').hide();
+					} else {
+						$('#advice').show();
 					}
 				}
-				advice();
+			}
+			advice();
 
-				$(window).resize(function() {
-					clearTimeout(v_t);
-					v_t = setTimeout(function() {
-						advice();
-					}, 10);
-				}).on("orientationchange", function(event) {
-					clearTimeout(v_t);
-					v_t = setTimeout(function() {
-						advice();
-					}, 10);
-				});
-			},
-			actionFun: function() { //页面动作
-				var touchLock = function() { //屏蔽上下弹动页面
-					$('#pageWrap').on('touchstart', function(event) {
-						event.preventDefault();
-					});
-				}
-				touchLock();
-				commonFun.viewportFix();
-				/*ico bottom*/
-				var b_t;
-				var bottomFun = function() {
-					$('#bottom').hide();
-					clearTimeout(b_t);
-					b_t = setTimeout(function() {
-						if (!$('.step-box').last().hasClass('show')) {
-							$('#bottom').fadeIn('slow');
-						};
-					}, 3000);
-				}
-				window.onload = function() {
-					if (document.readyState == 'complete') {
-						$('#loading').fadeOut('slow', function() {
-							$('.step-box').first().addClass('show').css('display', 'block').transition({
-								'opacity': '1'
-							});
-							commonFun.page_1();
+			$(window).resize(function() {
+				clearTimeout(v_t);
+				v_t = setTimeout(function() {
+					advice();
+				}, 10);
+			}).on("orientationchange", function(event) {
+				clearTimeout(v_t);
+				v_t = setTimeout(function() {
+					advice();
+				}, 10);
+			});
+		},
+		touchLock: function() { //屏蔽上下弹动页面
+			$('#pageWrap').on('touchstart', function(event) {
+				event.preventDefault();
+			});
+		},
+		swipeFun: function() { //手势上下滑动页面切换
+			$('.swipe-box').swipe({
+				swipeUp: function(event, direction, distance, duration, fingerCount) {
+					var that = $('.swipe-box.show'),
+						index = that.index(),
+						prev = that.prev(),
+						next = that.next();
+					if (index != $('.swipe-box').size() - 1) {
+						next.addClass('show').css({
+							'transform': 'scale(0.3) translate3d(0,80%,0)',
+							'display': 'block'
 						});
-						bottomFun();
-					}
+						that.removeClass('show').transition({
+							'scale': '0.3',
+							'y': '-80%',
+							'opacity': '0'
+						});
+						next.transition({
+							'scale': '1',
+							'y': '0',
+							'opacity': '1'
+						});
+					};
+					bottomFun();
+				},
+				swipeDown: function(event, direction, distance, duration, fingerCount) {
+					var that = $('.swipe-box.show'),
+						index = that.index(),
+						prev = that.prev(),
+						next = that.next();
+
+					if (index != 0) {
+						prev.addClass('show').css({
+							'transform': 'scale(0.3) translate3d(0,-80%,0)',
+							'display': 'block'
+						});
+						that.removeClass('show').transition({
+							'scale': '0.3',
+							'y': '80%',
+							'opacity': '0'
+						});
+						prev.transition({
+							'scale': '1',
+							'y': '0',
+							'opacity': '1'
+						});
+					};
 				}
-				$('.step-box').swipe({ //页面切换
-					swipeUp: function(event, direction, distance, duration, fingerCount) {
-						var that = $('.step-box.show'),
-							index = that.index(),
-							prev = that.prev(),
-							next = that.next();
-						if (index != $('.step-box').size() - 1) {
-							next.addClass('show').css({
-								'transform': 'scale(0.3) translate3d(0,80%,0)',
-								'display': 'block'
-							});
-							that.removeClass('show').transition({
-								'scale': '0.3',
-								'y': '-80%',
-								'opacity': '0'
-							});
-							next.transition({
-								'scale': '1',
-								'y': '0',
-								'opacity': '1'
-							});
-						};
-						bottomFun();
-					},
-					swipeDown: function(event, direction, distance, duration, fingerCount) {
-						var that = $('.step-box.show'),
-							index = that.index(),
-							prev = that.prev(),
-							next = that.next();
-
-						if (index != 0) {
-							prev.addClass('show').css({
-								'transform': 'scale(0.3) translate3d(0,-80%,0)',
-								'display': 'block'
-							});
-							that.removeClass('show').transition({
-								'scale': '0.3',
-								'y': '80%',
-								'opacity': '0'
-							});
-							prev.transition({
-								'scale': '1',
-								'y': '0',
-								'opacity': '1'
-							});
-						};
-					}
+			});
+		},
+		formatFun: function() { //页面初始化
+			commonFun.touchLock();
+			commonFun.viewportFix();
+			commonFun.pageLoad(function() {
+				$('.step-box').first().addClass('show').css('display', 'block').transition({
+					'opacity': '1'
 				});
-			},
-			page_1: function() {
-                var st = 200;
-                var objBox = $('.step-1');
-                objBox.find('.t').each(function(i) {
-                    var that = $(this);
-                    var fun = function() {
-                        that.addClass('x2d');
-                    }
-                    setTimeout(function() {
-                        fun();
-                    }, st * i);
-                });
-                setTimeout(function() {
-                    objBox.find('.t-s').fadeIn('slow');
-                }, 2000);
-            }
+				commonFun.page_logo();
+			});
+		},
+		page_logo: function() { //首屏LOGO
+			var objStep = $('.step-logo');
+			var closeLogo = function() {
+				objStep.fadeOut('slow');
+			}
+			var logo_mixzo = function() { //logo mixzo
+				objStep.find('.m-box').show();
+				var st = 200;
+				var objBox = $('.step-logo');
+				objBox.find('.t').each(function(i) {
+					var that = $(this);
+					var fun = function() {
+						that.addClass('x2d');
+					}
+					setTimeout(function() {
+						fun();
+					}, st * i);
+				});
+				setTimeout(function() {
+					objBox.find('.t-s').fadeIn('slow');
+				}, 2000);
+				setTimeout(function() {
+					objStep.find('.m-box').fadeOut('slow', function() {
+						closeLogo();
+						window.location.href = 'chapters-01.htm';
+					});
+				}, 5000);
+			}
+			logo_mixzo();
 		}
-		//共用调用
-	commonFun.actionFun();
+	}
+	commonFun.formatFun();
 	return commonFun;
-
 })(jQuery)
